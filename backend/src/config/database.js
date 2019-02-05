90a5fc9ad615fa29199f4861652bd77b6025841c
Alexis Sousa <alexis-sousa@hotmail.com>
2019-02-05 17:15:32 +0100

    test 1 change database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost/todo')
+module.exports = mongoose.connect('mongodb://localhost/todo2')
 
  
